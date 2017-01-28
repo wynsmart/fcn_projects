824100544a8d1d450ebef4fccd937ab7dc439f74
samkeets <samkeets@outlook.com>
2017-01-28 18:12:17 -0500

    class-ified

--- proj_2/crawl.py
+++ proj_2/crawl.py
@@ -1,31 +1,34 @@
 #! /usr/bin/env python
+from conns import MyPaw
 
-_visited = {}
-_unvisited = []
-_cookie = ""
-_flagcount = 0
+class CrawlCrawl(object):
+    def __init__(self):
+        self.fetch = MyPaw('malgorythmic','password').fetch()
+        self.flags = 0
+        self.visited = {}
+        self.unvisited = []
 
-def crawl(url):
-  global _visited
-  global _unvisited
+    def crawl(self, url):
 
-  # <a href=".+">(.*?)<\/a>
-  # <h2 class='secret_flag' style="color:red">(.*?)<\/h2>
+      # <a href=".+">(.*?)<\/a>
+      # <h2 class='secret_flag' style="color:red">(.*?)<\/h2>
 
-  while _unvisited is not None:
-    # HTTP GET
-    traverse_url = _unvisited[0]
-    _unvisited.remove(traverse_url)
-    _visited[url] = True
-    # data = HTTP GET traverse_url
-    # parse data into a string
-    # regex to get list of URLS
-    for url_a in regex_result:
-        if url_a in _visited:
-            continue
-        else:
-            unvisited.append(url_a)
-    # regex to get <h2 class="secret_flag"> tags & get flag and print
+      while self.unvisited is not None:
+        # HTTP GET
 
-    if _flagcount == 5:
-        break
+        traverse_url = self.unvisited[0]
+        self.unvisited.remove(traverse_url)
+        self.visited[traverse_url] = True
+
+        # data = HTTP GET traverse_url
+        # parse data into a string
+        # regex to get list of URLS
+        for url_a in regex_result:
+            if url_a in self.visited:
+                continue
+            else:
+                self.unvisited.append(url_a)
+        # regex to get <h2 class="secret_flag"> tags & get flag and print
+
+        if self.flags == 5:
+            break
