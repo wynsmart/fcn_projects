2ead46aeeb9944f065a62064068eee142f050841
Fan Yang <wyns.smart@gmail.com>
2017-04-07 22:30:28 -0400

    4.7.1
    - file lock

--- proj_5/httpserver.py
+++ proj_5/httpserver.py
@@ -17,17 +17,20 @@ class MyServer:
         self.originPort = 8080
         self.maxConn = 9999
         self.threads = []
-        self.cache = MyCache()
+        self.lock = MyFileLock()
+        self.cache = MyCache(self.lock)
+        utils.log('port   ->', self.port)
+        utils.log('origin ->', self.originHost)
 
     def start(self):
         sock = socket.socket()
         try:
             sock.bind((self.ip, self.port))
             sock.listen(5)
-            utils.log('HTTP Server -> {}'.format(sock.getsockname()))
         except Exception as e:
             exit(e)
 
+        utils.log('servering ...')
         while 1:
             self.threads = [t for t in self.threads if t.is_alive()]
             if len(self.threads) >= self.maxConn:
@@ -35,16 +38,6 @@ class MyServer:
             conn, _ = sock.accept()
             handler = MyReqHandler(self, conn)
             handler.start()
-            # utils.log(conn.getpeername(), 'connect')
-            # handler = MyReqHandler(self, conn)
-            # if handler.method != 'GET':
-            #     handler.done()
-            #     utils.log('only GET method is supported')
-            #     continue
-            # t = threading.Thread()
-            # t.daemon = True
-            # t.run = handler.do_GET
-            # t.start()
             self.threads.append(handler)
 
 
@@ -60,11 +53,11 @@ class MyReqHandler(threading.Thread):
     def run(self):
         raw_request = self.conn.recv(4096)
         self.req = MyRequest(raw_request)
-        utils.log(self.client, self.req.method, self.req.path)
         if self.req.method != 'GET':
             self.done()
             utils.log('only GET method is supported')
             exit()
+        utils.log(self.client, self.req.method, self.req.path)
         self.do_GET()
 
     def do_GET(self):
@@ -82,15 +75,18 @@ class MyReqHandler(threading.Thread):
 
     def prep_response(self):
         cached_res = self.server.cache.get(self.req.path)
-        if cached_res is None:
-            res = self.fetch_origin()
-            self.server.cache.set(self.req.path, res)
-            return res
-        else:
+        if cached_res is not None:
             utils.log(self.client, 'respond with cache')
             return cached_res
+        # fetch origin
+        utils.log(self.client, 'fetch origin', self.req.path)
+        res = self.fetch_origin()
+        self.server.cache.set(self.req.path, res)
+        return res
+
 
     def fetch_origin(self):
+        # TODO: retry when failure with timeouted socket
         host = self.server.originHost
         port = self.server.originPort
         raw_request = self.req.forward_raw(host, port)
@@ -100,6 +96,7 @@ class MyReqHandler(threading.Thread):
         res = bytes()
         while not self.res_complete(res):
             res += sock.recv(4096)
+        sock.close()
         return res
 
     def res_complete(self, res):
@@ -144,8 +141,9 @@ class MyRequest:
 
 class MyCache:
     # TODO: Implement in memory cache as well
-    def __init__(self):
+    def __init__(self, lock):
         # TODO: don't cache dynamic pages such as Special:Random
+        self.lock = lock
         self.dir = './cache'
         if not os.path.isdir(self.dir):
             os.makedirs(self.dir)
@@ -158,29 +156,48 @@ class MyCache:
         # TODO: test and handle db concurrent writes
         fname = self.get_fname(path)
         res_lite = zlib.compress(res)
+        self.lock.acquire(fname)
         with open(fname, mode='wb') as f:
             f.write(res_lite)
-        utils.log('cached:', path, fname)
+        self.lock.release(fname)
+        utils.log('cached:', path, '->', fname)
 
     def get(self, path):
         fname = self.get_fname(path)
+        self.lock.acquire(fname)
         try:
             with open(fname, mode='rb') as f:
                 res_lite = f.read()
         except Exception as e:
             utils.log(e)
             return None
+        finally:
+            self.lock.release(fname)
         res = zlib.decompress(res_lite)
         return res
 
-    def drop(self, url):
-        conn = sqlite3.connect(self.dbname)
-        c = conn.cursor()
-        c.execute("""
-        delete from `{}` where `url`=?
-        """.format(self.tablename), (url, ))
-        conn.commit()
+    def drop(self, path):
+        fname = self.get_fname(path)
+        self.lock.acquire(fname)
+        try:
+            os.remove(fname)
+        except Exception as e:
+            utils.log(e)
+        self.lock.release(fname)
 
+class MyFileLock:
+    def __init__(self):
+        self.locks = set()
+
+    def acquire(self, fname):
+        while fname in self.locks:
+            utils.log('xxx')
+            pass
+        self.locks.add(fname)
+        assert fname in self.locks
+
+    def release(self, fname):
+        self.locks.remove(fname)
 
 def main():
     port = utils.args.port
